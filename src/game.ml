0e5c73353c69a11ef3c36b330634f50cf0f4ce04
Julie Jeong <sj598@cornell.edu>
2021-10-20 22:10:13 -0400

    added functions in game.ml (work done by Aurora)

--- src/game.ml
+++ src/game.ml
@@ -2,7 +2,6 @@ open Player
 open Tile 
 open TileStack 
 
-
 type t = {
     banker : Player.t  
     Center_Tiles : TileStack 
@@ -10,3 +9,20 @@ type t = {
     Discard_Tile : List 
     Players : Player.t list   
 }
+
+let rec assign_banker players n = {
+  match players with
+  | h::t -> if h.id == n then h.id == True else assign_banker t n
+}
+ 
+let assign_tiles player center = {
+  let lst = [] in
+  match player with
+  | h::t
+}
+ 
+let rec filter tilelst = function
+| [] -> []
+| h :: t -> if is_flower tile = false then h :: is_flower t else is_flower t
+ 
+let check_bankertile player tile = if player.is_banker && tiles.length <= 12 then add_tile player tile
