f2c72780beb88e2f73ef0a8cb3fe5164463a3927
Ruike Liang <rl582@cornell.edu>
2021-10-20 22:50:53 -0400

    updated game.mli for shuffle_tiles

--- src/game.mli
+++ src/game.mli
@@ -1,2 +1,5 @@
 type t 
 (** The abstract type of values representing a game *)
+
+val shuffle_tiles : a' list -> a' list 
+(** [shuffle_tiles tile_list] takes in a tileStack and returns that tileStack shuffled*)
