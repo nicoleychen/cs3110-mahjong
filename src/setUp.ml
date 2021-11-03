52e63c471d4144b8113e0ca09b08e1341f6bcf9c
Ruike Liang <rl582@cornell.edu>
2021-11-02 23:04:52 -0400

    removed setUp

--- src/setUp.ml
+++
@@ -1,9 +0,0 @@
-open Tile
-open TileStack
-open Player
-open Game
-
-(** n should be 143 
-let rec init_tiles (n:int) : TileStack.t =
-  if (n>=0) then return_tile i |> (init_tiles n-1).push else TileStack.empty
-*)
