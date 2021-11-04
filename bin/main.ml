d4c80938a060356a846912e51cf532299b53e563
Aurora Mu <aym32@cornell.edu>
2021-11-03 22:12:08 -0400

    main error

--- bin/main.ml
+++ bin/main.ml
@@ -15,17 +15,12 @@ let main () =
     print_string "> ";
   match read_line () with
   | exception End_of_file -> ()
-<<<<<<< HEAD
-  | answer -> if (answer = "Y") then setup_game else print_endline "Okay, see you later!";
+  | answer -> if (answer = "Y") then setup_game () else print_endline "Okay, see you later!";
   ANSITerminal.print_string [ANSITerminal.green]
   "Insert Player 1 Name: ";
   match read_line () with 
   | exception End_of_file -> ()
   | answer -> 
-
-=======
-  | answer -> if (answer = "Y") then setup_game () else print_endline "Okay, see you later!"
->>>>>>> bcd5bdbcafed97fbee0b255b3c80880ce831dc84
 
 (* Execute the game engine. *)
 (* let () = print_endline "hi" *)
