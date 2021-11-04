77fc8d53586438bdd47334cb3b6a8bbbe367a8e9
juliejeong <julie.seoyeon.jeong@gmail.com>
2021-11-03 22:38:21 -0400

    added specifications

--- src/player.mli
+++ src/player.mli
@@ -16,5 +16,7 @@ val get_tiles : t -> TileStack.t
 (** [get_titles] is the list of [player]'s tiles *)
 
 val player_to_string: t -> string
+(** [player_to_string] is a string of [player]'s info including id, name, and tiles*)
 
 val players_to_string: t list -> string
+(** [players_to_string] is a string of a list of [player]'s info including id, name, and tiles*)
